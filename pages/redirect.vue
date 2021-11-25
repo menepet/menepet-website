52ea6ab08f5509e36284f82e0885ec667bc3bec7
Mene Pet <menepet@gmail.com>
2021-11-25 22:54:33 +0200

    redirect test to new domain

---
+++ pages/redirect.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Redirect Page to new Domain
+  </div>
+</template>
+
+<script>
+import { metaHead } from '~/utils'
+
+export default {
+  name: 'Redirect',
+  asyncData({ redirect }) {
+    return redirect('https://mene.pet')
+  },
+  head() {
+    return {
+      ...metaHead('Redirect', 'My Redirect Projects')
+    }
+  }
+}
+</script>
